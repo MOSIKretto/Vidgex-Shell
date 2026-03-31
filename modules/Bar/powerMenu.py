f42db9312daf33932b55fa8282981a6d037f835a
Max <149097230+MOSIKretto@users.noreply.github.com>
2026-03-31 03:16:03 +0300

    power menu

--- modules/Bar/powerMenu.py
+++ modules/Bar/powerMenu.py
@@ -11,31 +11,14 @@ from gi.repository import Gdk, GLib
 from services.wayland import WaylandWindow as Window
 import services.icons as icons
 
-def _hand_cursor(widget):
-    def _set(w, _):
-        win = w.get_window()
-        if win: win.set_cursor(Gdk.Cursor.new_from_name(w.get_display(), "pointer"))
-        return False
-
-    def _reset(w, _):
-        win = w.get_window()
-        if win: win.set_cursor(Gdk.Cursor.new_from_name(w.get_display(), "default"))
-        return False
-
-    widget.add_events(Gdk.EventMask.ENTER_NOTIFY_MASK | Gdk.EventMask.LEAVE_NOTIFY_MASK | Gdk.EventMask.BUTTON_PRESS_MASK | Gdk.EventMask.BUTTON_RELEASE_MASK)
-    widget.connect("enter-notify-event", _set)
-    widget.connect("leave-notify-event", _reset)
-    widget.connect("button-press-event", _set)
-    widget.connect("button-release-event", _set)
-
 
 class PowerMenu(Window):
-    _ACTIONS = [        
+    _ACTIONS = (
         (icons.shutdown, "Shutdown", "systemctl poweroff"),
-        (icons.reboot, "Reboot", "systemctl reboot"),
-        (icons.logout, "Logout", "hyprctl dispatch exit"),
-        (icons.suspend, "Suspend", "systemctl suspend"),
-    ]
+        (icons.reboot,   "Reboot",   "systemctl reboot"),
+        (icons.logout,   "Logout",   "hyprctl dispatch exit"),
+        (icons.suspend,  "Suspend",  "systemctl suspend"),
+    )
 
     def __init__(self, monitor=0, **kw):
         super().__init__(
@@ -45,35 +28,56 @@ class PowerMenu(Window):
             keyboard_mode="none",
         )
         self.set_name("power-menu-window")
-        
-        GLib.idle_add(lambda: exec_shell_command_async('hyprctl keyword layerrule "noanim, power-menu-window"'))
-        
         self.anchor = "top right"
         self.margin = "0px 0px 0px 0px"
 
-        self.set_app_paintable(True)
         visual = self.get_screen().get_rgba_visual()
         if visual:
+            self.set_app_paintable(True)
             self.set_visual(visual)
 
         self._open_flag = False
-        self._close_timer = None
+        self._close_timer = 0
+        self._reveal_timer = 0
+        self._trigger_btn = None
+        self._trigger_sig = 0
+        self._hand = None
+        self._default = None
+
         self._btns = []
         self._revealers = []
-        self._trigger_btn = None
-        
-        self._dyn_width = 44
-        self._dyn_height = 44
-
         self._build_ui()
         self.set_visible(False)
 
+        exec_shell_command_async('hyprctl keyword layerrule "noanim, power-menu-window"')
+
+    def _ensure_cursors(self):
+        if self._hand is None:
+            display = self.get_display()
+            self._hand = Gdk.Cursor.new_from_name(display, "pointer")
+            self._default = Gdk.Cursor.new_from_name(display, "default")
+
+    def _set_toplevel_cursor(self, cursor):
+        toplevel = self.get_toplevel()
+        if toplevel:
+            win = toplevel.get_window()
+            if win:
+                win.set_cursor(cursor)
+
+    def _on_btn_enter(self, w, event):
+        if event.detail != Gdk.NotifyType.INFERIOR:
+            self._ensure_cursors()
+            self._set_toplevel_cursor(self._hand)
+        return False
+
+    def _on_btn_leave(self, w, event):
+        if event.detail != Gdk.NotifyType.INFERIOR:
+            self._ensure_cursors()
+            self._set_toplevel_cursor(self._default)
+        return False
+
     def _build_ui(self):
-        self._icons_box = Box(
-            name="power-menu-icons",
-            orientation="v",
-            spacing=4,
-        )
+        icons_box = Box(name="power-menu-icons", orientation="v", spacing=4)
 
         for icon_markup, tooltip, cmd in self._ACTIONS:
             btn = Button(
@@ -81,14 +85,15 @@ class PowerMenu(Window):
                 tooltip_markup=tooltip,
                 can_focus=False,
             )
-            sc = btn.get_style_context()
-            sc.add_class("power-icon-btn")
-
-            btn.connect("clicked", lambda _, c=cmd: self._execute(c))
-            _hand_cursor(btn)
-
+            btn.get_style_context().add_class("power-icon-btn")
+            btn.add_events(
+                Gdk.EventMask.ENTER_NOTIFY_MASK | Gdk.EventMask.LEAVE_NOTIFY_MASK
+            )
+            btn.connect("enter-notify-event", self._on_btn_enter)
+            btn.connect("leave-notify-event", self._on_btn_leave)
+            btn.connect("clicked", self._on_clicked, cmd)
             self._btns.append(btn)
-            
+
             rev = Revealer(
                 transition_type="slide-down",
                 child_revealed=False,
@@ -96,107 +101,116 @@ class PowerMenu(Window):
             )
             rev.set_transition_duration(200)
             self._revealers.append(rev)
-            self._icons_box.add(rev)
+            icons_box.add(rev)
 
-        self._menu_wrapper = Box(
+        self._wrapper = Box(
             name="power-menu-wrapper",
             orientation="v",
-            children=[self._icons_box],
+            children=[icons_box],
         )
-
-        self.add(self._menu_wrapper)
+        self.add(self._wrapper)
 
     def set_trigger_button(self, btn):
+        if self._trigger_btn and self._trigger_sig:
+            self._trigger_btn.disconnect(self._trigger_sig)
         self._trigger_btn = btn
-        self._trigger_btn.connect("size-allocate", self._on_trigger_allocate)
+        self._trigger_sig = btn.connect("size-allocate", self._on_trigger_alloc)
 
-    def _on_trigger_allocate(self, widget, allocation):
-        if allocation.width > 0 and allocation.height > 0:
-            self._dyn_width = allocation.width
-            self._dyn_height = allocation.height
-            
-            for b in self._btns:
-                b.set_size_request(self._dyn_width, self._dyn_height)
-                
-            total_h = (self._dyn_height * len(self._btns)) + (4 * (len(self._btns) - 1))
-            self._menu_wrapper.set_size_request(self._dyn_width, total_h)
-                
-            toplevel = widget.get_toplevel()
-            if toplevel:
-                ok, x, y = widget.translate_coordinates(toplevel, 0, 0)
-                if ok:
-                    top_w = toplevel.get_allocated_width()
-                    
-                    base_right = int(top_w - (x + self._dyn_width))
-                    base_top = int(y + self._dyn_height) + 4
-                    
-                    OFFSET_RIGHT = 0  
-                    OFFSET_TOP = 0    
-                    
-                    m_right = base_right + OFFSET_RIGHT
-                    m_top = base_top + OFFSET_TOP
-                    
-                    self.margin = f"{m_top}px {m_right}px 0px 0px"
+    def _on_trigger_alloc(self, widget, alloc):
+        w, h = alloc.width, alloc.height
+        if w <= 0 or h <= 0:
+            return
 
-    def _set_trigger_danger(self, danger):
-        if self._trigger_btn is None: return
-        sc = self._trigger_btn.get_style_context()
-        if danger: sc.add_class("power-active")
-        else: sc.remove_class("power-active")
+        for b in self._btns:
+            b.set_size_request(w, h)
+
+        n = len(self._ACTIONS)
+        self._wrapper.set_size_request(w, h * n + 4 * (n - 1))
+
+        toplevel = widget.get_toplevel()
+        if not toplevel:
+            return
+
+        ok, x, y = widget.translate_coordinates(toplevel, 0, 0)
+        if not ok:
+            return
+
+        m_right = toplevel.get_allocated_width() - x - w
+        m_top = y + h + 4
+        self.margin = f"{m_top}px {m_right}px 0px 0px"
 
     def open(self):
-        self._cancel()
+        if self._open_flag:
+            return
+        self._cancel_all()
         self._open_flag = True
-        self._set_trigger_danger(True)
+        self._set_danger(True)
+
+        for rev in self._revealers:
+            rev.set_reveal_child(False)
 
         self.set_visible(True)
         self.show_all()
-        
-        for rev in self._revealers:
-            rev.set_reveal_child(False)
-        
-        GLib.timeout_add(30, self._do_reveal_step, 0)
+        self._reveal_step(0)
 
-    def _do_reveal_step(self, idx):
+    def _reveal_step(self, idx):
+        self._reveal_timer = 0
         if not self._open_flag or idx >= len(self._revealers):
             return False
         self._revealers[idx].set_reveal_child(True)
-        GLib.timeout_add(45, self._do_reveal_step, idx + 1)
+        if idx + 1 < len(self._revealers):
+            self._reveal_timer = GLib.timeout_add(45, self._reveal_step, idx + 1)
         return False
 
     def close(self):
-        self._cancel()
-        if not self._open_flag: return
+        if not self._open_flag:
+            return
+        self._cancel_all()
         self._open_flag = False
-        self._set_trigger_danger(False)
-        
+        self._set_danger(False)
+
         for rev in self._revealers:
             rev.set_reveal_child(False)
-            
+
         self._close_timer = GLib.timeout_add(250, self._hide)
 
     def _hide(self):
-        self._close_timer = None
+        self._close_timer = 0
         if not self._open_flag:
             self.set_visible(False)
         return False
 
-    def _cancel(self):
+    def _cancel_all(self):
         if self._close_timer:
             GLib.source_remove(self._close_timer)
-            self._close_timer = None
+            self._close_timer = 0
+        if self._reveal_timer:
+            GLib.source_remove(self._reveal_timer)
+            self._reveal_timer = 0
 
-    def is_open(self): return self._open_flag
+    def _set_danger(self, active):
+        if not self._trigger_btn:
+            return
+        sc = self._trigger_btn.get_style_context()
+        if active:
+            sc.add_class("power-active")
+        else:
+            sc.remove_class("power-active")
 
-    def _execute(self, cmd):
-        try: exec_shell_command_async(cmd)
-        except Exception: pass
+    def is_open(self):
+        return self._open_flag
+
+    def _on_clicked(self, _btn, cmd):
         self.close()
+        exec_shell_command_async(cmd)
 
     def cleanup(self):
-        self._cancel()
-        self._set_trigger_danger(False)
+        self._cancel_all()
+        self._set_danger(False)
         if self._open_flag:
             self._open_flag = False
             self.set_visible(False)
+        if self._trigger_btn and self._trigger_sig:
+            self._trigger_btn.disconnect(self._trigger_sig)
+            self._trigger_sig = 0
         self._trigger_btn = None
--- modules/bar.py
+++ modules/bar.py
@@ -1,3 +1,5 @@
+import weakref
+
 from fabric.hyprland.widgets import (
     HyprlandLanguage as Language,
     get_hyprland_connection,
@@ -24,40 +26,22 @@ from services.wayland import WaylandWindow as Window
 import services.icons as icons
 
 
-def _hand_cursor(widget):
-    def _set(w, _):
-        win = w.get_window()
-        if win: win.set_cursor(Gdk.Cursor.new_from_name(w.get_display(), "pointer"))
-        return False
-
-    def _reset(w, _):
-        win = w.get_window()
-        if win: win.set_cursor(Gdk.Cursor.new_from_name(w.get_display(), "default"))
-        return False
-
-    widget.add_events(Gdk.EventMask.ENTER_NOTIFY_MASK | Gdk.EventMask.LEAVE_NOTIFY_MASK | Gdk.EventMask.BUTTON_PRESS_MASK | Gdk.EventMask.BUTTON_RELEASE_MASK)
-    widget.connect("enter-notify-event", _set)
-    widget.connect("leave-notify-event", _reset)
-    widget.connect("button-press-event", _set)
-    widget.connect("button-release-event", _set)
-
-
 class Bar(Window):
-    __slots__ = (
-        "mid", "notch", "lang", "conn", "ws", "wsc",
-        "tray", "rl", "met", "rr", "ll", "dt", "bat", "bp",
-        "sidebar", "power_menu"
-    )
-
     def __init__(self, monitor_id=0, **kwargs):
         super().__init__(exclusivity="auto", monitor_id=monitor_id)
         self.mid = monitor_id
-        self.notch = kwargs.get("notch")
+
+        notch = kwargs.get("notch")
+        self._notch_ref = weakref.ref(notch) if notch else None
+
         self.anchor = "left top right"
         self.margin = "-4px -4px -8px -4px"
 
-        self.lang = Language()
         self.conn = get_hyprland_connection()
+        self.lang = Language()
+        self._last_lang = ""
+        self._hand = None
+        self._default = None
 
         self.sidebar = SideBarWindow(conn=self.conn, monitor_id=self.mid)
         self.sidebar.show_all()
@@ -65,22 +49,65 @@ class Bar(Window):
         self.power_menu = PowerMenu(monitor=self.mid)
 
         self._build()
-        self.lang.connect("notify::label", self._lchg)
+        self._lang_sig_id = self.lang.connect("notify::label", self._lchg)
         self._lchg()
+
+    @property
+    def notch(self):
+        return self._notch_ref() if self._notch_ref else None
+
+    @notch.setter
+    def notch(self, value):
+        self._notch_ref = weakref.ref(value) if value else None
+
+    def _ensure_cursors(self):
+        if self._hand is None:
+            display = self.get_display()
+            self._hand = Gdk.Cursor.new_from_name(display, "pointer")
+            self._default = Gdk.Cursor.new_from_name(display, "default")
+
+    def _set_toplevel_cursor(self, cursor):
+        toplevel = self.get_toplevel()
+        if toplevel:
+            win = toplevel.get_window()
+            if win:
+                win.set_cursor(cursor)
+
+    def _on_btn_enter(self, w, event):
+        if event.detail != Gdk.NotifyType.INFERIOR:
+            self._ensure_cursors()
+            self._set_toplevel_cursor(self._hand)
+        return False
+
+    def _on_btn_leave(self, w, event):
+        if event.detail != Gdk.NotifyType.INFERIOR:
+            self._ensure_cursors()
+            self._set_toplevel_cursor(self._default)
+        return False
+
+    def _hand_cursor(self, widget):
+        widget.add_events(
+            Gdk.EventMask.ENTER_NOTIFY_MASK | Gdk.EventMask.LEAVE_NOTIFY_MASK
+        )
+        widget.connect("enter-notify-event", self._on_btn_enter)
+        widget.connect("leave-notify-event", self._on_btn_leave)
 
     def _build(self):
         self.ws = TopWorkspaces(conn=self.conn, v_align="center", h_align="start")
-
         self.tray = SystemTray()
         self.met = MetricsSmall()
 
         self.rl = Revealer(
-            name="bar-revealer", transition_type="slide-right", child_revealed=True,
+            name="bar-revealer",
+            transition_type="slide-right",
+            child_revealed=True,
             child=Box(name="bar-revealer-box", spacing=4, children=[self.tray]),
         )
 
         self.rr = Revealer(
-            name="bar-revealer", transition_type="slide-left", child_revealed=True,
+            name="bar-revealer",
+            transition_type="slide-left",
+            child_revealed=True,
             child=Box(name="bar-revealer-box", spacing=4, children=[self.met]),
         )
 
@@ -88,75 +115,107 @@ class Bar(Window):
         self.bat = Battery()
 
         self.bp = Button(
-            name="button-bar", tooltip_markup="<b>Power menu</b>",
-            on_clicked=self._pwr, child=Label(name="button-bar-label", markup=icons.shutdown),
+            name="button-bar",
+            tooltip_markup="<b>Power menu</b>",
+            on_clicked=self._pwr,
+            child=Label(name="button-bar-label", markup=icons.shutdown),
         )
         _hov(self.bp)
-        _hand_cursor(self.bp)
-        
+        self._hand_cursor(self.bp)
         self.power_menu.set_trigger_button(self.bp)
 
         self.ll = Label(name="lang-label")
-
         self.autolayout_btn = AutolayoutButton()
 
         self.lang_revealer = Revealer(
             name="lang-revealer",
             transition_type="slide-right",
             child_revealed=False,
-            child=self.autolayout_btn
+            child=self.autolayout_btn,
         )
 
-        self.lang_box = Box(
-            name="language-indicator",
-            spacing=4,
-            children=[self.lang_revealer, self.ll]
+        self.lang_eb = EventBox(
+            child=Box(
+                name="language-indicator",
+                spacing=4,
+                children=[self.lang_revealer, self.ll],
+            )
         )
 
-        self.lang_eb = EventBox(child=self.lang_box)
+        self.lang_eb.connect("enter-notify-event", self._lang_enter)
+        self.lang_eb.connect("leave-notify-event", self._lang_leave)
 
-        self.lang_eb.connect("enter-notify-event", lambda *_: self.lang_revealer.set_reveal_child(True))
+        self.add(
+            CenterBox(
+                name="bar-inner",
+                start_children=Box(
+                    name="start-container",
+                    spacing=4,
+                    children=[
+                        self.ws,
+                        Box(name="boxed-revealer", children=[self.rl]),
+                    ],
+                ),
+                end_children=Box(
+                    name="end-container",
+                    spacing=4,
+                    children=[
+                        Box(name="boxed-revealer", children=[self.rr]),
+                        Box(
+                            name="power-battery-container",
+                            children=[self.dt, self.lang_eb, self.bat, self.bp],
+                        ),
+                    ],
+                ),
+            )
+        )
 
-        def _on_lang_leave(widget, event):
-            if event.detail != Gdk.NotifyType.INFERIOR:
-                self.lang_revealer.set_reveal_child(False)
-            return False
+    def _lang_enter(self, w, event):
+        if event.detail != Gdk.NotifyType.INFERIOR:
+            self.lang_revealer.set_reveal_child(True)
+        return False
 
-        self.lang_eb.connect("leave-notify-event", _on_lang_leave)
-
-        self.add(CenterBox(
-            name="bar-inner",
-            start_children=Box(
-                name="start-container", spacing=4,
-                children=[self.ws, Box(name="boxed-revealer", children=[self.rl])]
-            ),
-            end_children=Box(
-                name="end-container", spacing=4,
-                children=[
-                    Box(name="boxed-revealer", children=[self.rr]),
-                    Box(name="power-battery-container", children=[self.dt, self.lang_eb, self.bat, self.bp])
-                ]
-            ),
-        ))
+    def _lang_leave(self, w, event):
+        if event.detail != Gdk.NotifyType.INFERIOR:
+            self.lang_revealer.set_reveal_child(False)
+        return False
 
     def _lchg(self, *_):
-        if l := self.lang.get_label():
-            self.ll.set_label(l[:3].upper())
+        raw = self.lang.get_label()
+        if not raw:
+            return
+        short = raw[:3].upper()
+        if short != self._last_lang:
+            self._last_lang = short
+            self.ll.set_label(short)
 
     def _pwr(self, *_):
-        if self.power_menu.is_open():
-            self.power_menu.close()
-        else:
-            self.power_menu.open()
+        pm = self.power_menu
+        if pm:
+            if pm.is_open():
+                pm.close()
+            else:
+                pm.open()
 
     def cleanup(self):
+        if self.lang and self._lang_sig_id:
+            self.lang.disconnect(self._lang_sig_id)
+            self._lang_sig_id = 0
+
         if self.sidebar:
             self.sidebar.destroy()
             self.sidebar = None
+
         if self.power_menu:
             self.power_menu.cleanup()
             self.power_menu.destroy()
             self.power_menu = None
-        if hasattr(self.tray, 'cleanup'): self.tray.cleanup()
-        if hasattr(self.bat, 'cleanup'): self.bat.cleanup()
-        self.notch = self.conn = self.lang = None
+
+        if hasattr(self.tray, "cleanup"):
+            self.tray.cleanup()
+        if hasattr(self.bat, "cleanup"):
+            self.bat.cleanup()
+
+        self._notch_ref = None
+        self.conn = None
+        self.lang = None
